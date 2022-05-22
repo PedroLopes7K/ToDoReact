d5a9429b2d0391b10c51689266d07241885f1e3e
PedroLopes7K <pedrohenri3601@gmail.com>
2022-05-22 16:40:19 -0300

    Add delete function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 // import logo from './logo.svg'
 import './App.css'
 import { useState } from 'react'
-import Task from './Components/Task'
+// import Task from './Components/Task'
 import { Button } from './Components/StyledComponents'
 import { Input } from './Components/StyledComponents'
+import { Card } from './Components/StyledComponents'
 
 function App() {
   const [taskName, setTaskName] = useState('')
-  const [time, setTime] = useState('')
+  // const [time, setTime] = useState('')
   const [taskList, setTaskList] = useState([])
 
-  const addTask = () => {
-    setTaskList([...taskList, { task: taskName, time: time }])
-    console.log(taskList)
-    setTaskName('')
-    setTime('')
+  const addTask = task => {
+    setTaskList([...taskList, task])
+    // console.log(taskList.length)
+  }
+  function del(taskDel) {
+    console.log(taskDel)
+    const result = taskList.filter(task => {
+      return task != taskDel
+    })
+    setTaskList(result)
+    // for (i; i < taskList.length; i++) {
+    //   if (taskList[i] === taskDel) {
+    //     setTaskList([taskList.splice(i, 1)])
+    //     let newArray = taskList.splice(i, 1)
+    //     setTaskList(newArray)
+    //     return
+    //   }
+    // }
+  }
+
+  function edit() {
+    alert('Editar')
   }
 
   return (
@@ -27,19 +45,23 @@ function App() {
           setTaskName(e.target.value)
         }}
       />
-      <label>Time:</label>
-      <Input
-        type="text"
-        onChange={e => {
-          setTime(e.target.value)
-        }}
-      />
 
-      <Button onClick={addTask}>Add Task</Button>
+      <Button onClick={() => addTask(taskName)}>Add Task</Button>
       <hr color="gray" size="2" width="50%" />
 
       {taskList.map(taskItem => (
-        <Task nameTask={taskItem.task} time={taskItem.time} />
+        <Card background={'gray'} width={'250px'} height={'120px'}>
+          <h4>Task: {taskItem}</h4>
+          <button
+            onClick={() => del(taskItem)}
+            style={{ background: 'red', margin: '0 10px' }}
+          >
+            Excluir
+          </button>
+          <button onClick={edit} style={{ background: 'blue' }}>
+            Editar
+          </button>
+        </Card>
       ))}
     </div>
   )
